8af7d7bc6b643f33215b1c30c65a5fe095dae294
DESKTOP-MPNG05G\Service Unit <fsuller@yondu.com>
2021-12-07 20:56:34 +0800

    Update: Sensor shake applied, video on pause

--- app/src/main/java/com/mobile/andrioidpracticalexamexoplayer/MainActivity.kt
+++ app/src/main/java/com/mobile/andrioidpracticalexamexoplayer/MainActivity.kt
@@ -1,9 +1,23 @@
 package com.mobile.andrioidpracticalexamexoplayer
 
+import android.Manifest
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
@@ -11,9 +25,18 @@ import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.util.Log
 import com.google.android.exoplayer2.util.Util
+import com.google.android.gms.location.*
 import com.mobile.andrioidpracticalexamexoplayer.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.math.sqrt
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_CHECK_SETTINGS = 1
+        private const val REQUEST_LOCATION = 0
+    }
 
     private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
         ActivityMainBinding.inflate(layoutInflater)
@@ -26,6 +49,17 @@ class MainActivity : AppCompatActivity() {
     private var playbackPosition = 0L
 
     private val playbackStateListener: Player.EventListener = playbackStateListener()
+
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+
+ //   private val locationVm: LocationSharedViewModel by viewModels()
+
+    private lateinit var sensorManager: SensorManager
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
+
+    private val PERMISSION_ID = 44
     private fun initializePlayer() {
         val trackSelector = DefaultTrackSelector(this).apply {
             setParameters(buildUponParameters().setMaxVideoSizeSd())
@@ -50,20 +84,50 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(viewBinding.root)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        Objects.requireNonNull(sensorManager).registerListener(sensorListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+        acceleration = 10f
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        lastAcceleration = SensorManager.GRAVITY_EARTH
+        // method to get the location
+
+
+    }
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+            currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+            if (acceleration > 12) {
+                player?.playWhenReady = false
+                player?.playbackState
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+
     public override fun onResume() {
         super.onResume()
         hideSystemUi()
         if ((Util.SDK_INT < 24 || player == null)) {
             initializePlayer()
         }
-    }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(viewBinding.root)
+
+        sensorManager.registerListener(sensorListener, sensorManager.getDefaultSensor(
+            Sensor .TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL
+        )
     }
 
     public override fun onPause() {
         super.onPause()
+        sensorManager.unregisterListener(sensorListener)
         if (Util.SDK_INT < 24) {
             releasePlayer()
         }
