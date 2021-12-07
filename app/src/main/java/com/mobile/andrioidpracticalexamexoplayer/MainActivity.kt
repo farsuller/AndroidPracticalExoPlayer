4166b39892ea0fbeb8ae9c4bda6e28d64510dfe5
DESKTOP-MPNG05G\Service Unit <fsuller@yondu.com>
2021-12-07 13:05:29 +0800

    Update: Gradle and Manifest updates

--- app/src/main/java/com/mobile/andrioidpracticalexamexoplayer/MainActivity.kt
+++ app/src/main/java/com/mobile/andrioidpracticalexamexoplayer/MainActivity.kt
@@ -2,15 +2,16 @@ package com.mobile.andrioidpracticalexamexoplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mobile.andrioidpracticalexamexoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
-        ActivityPlayerBinding.inflate(layoutInflater)
+        ActivityMainBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
     }
 }
